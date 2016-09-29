7210c7bc319bde415e858cc75c2bc36cc42e35f9
Marisa Oliva <marisa.oliva@macpro-marisa-oliva.local>
2016-09-29 16:05:46 +0200

    delete gulp and include karma

--- gulp/tasks/images.js
+++
@@ -1,7 +0,0 @@
-const gulp      = require('gulp');
-const config    = require('../config');
-
-gulp.task('images', function(done) {
-    return gulp.src([config.paths.src+'/assets/images/**/*'])
-        .pipe(gulp.dest(config.paths.dist+'/assets/images'));
-});
---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = {};
+
+module.exports = function(config) {
+  config.set({
+    // ... normal karma configuration
+    files: [
+      'dist/bundle.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      {pattern: 'test/*.spec.js'}
+
+      // each file acts as entry point for the webpack configuration
+    ],
+    browsers: ['PhantomJS'],
+    frameworks: ['jasmine'],
+    reporters: ['coverage'],
+    preprocessors: {
+      // add webpack as preprocessor
+      './app/bundle.js': ['webpack'],
+      './test/*.spec.js': ['webpack', 'coverage']
+
+    },
+    coverageReporter: {
+      reporters: [
+          {
+              type: 'text-summary',
+          },
+          {
+              type: 'html',
+              dir: 'coverage/',
+          }
+      ]
+  },
+    webpack: webpackConfig,
+
+     webpackMiddleware: {
+       noInfo: true
+     }
+  });
+};
