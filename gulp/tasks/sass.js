1525082ad8a54533b288777ee6297605df83fdb1
Jorge Lucas <jorge.lucas@macpro-jorge-lucas.local>
2016-11-16 17:25:54 +0100

    auto-pixel-to-rem

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -8,6 +8,20 @@ const rename        = require('gulp-rename');
 const combiner      = require('stream-combiner2');
 const config        = require('../config');
 const error         = require('../error');
+const pxtorem       = require('gulp-pxtorem');
+const pxtoremOptions = {
+    replace: true,
+    prop_white_list: [
+                'width','min-width', 'max-width',
+                'height', 'min-height', 'max-height',
+                'margin', 'margin-left', 'margin-right', 'margin-top', 'margin-bottom',
+                'padding', 'padding-left', 'padding-right', 'padding-top', 'padding-bottom',
+                'font', 'font-size', 'line-height', 'letter-spacing'],
+};
+ 
+const postcssOptions = {
+    map: true  
+};
 
 
 gulp.task('sass', function () {
@@ -19,6 +33,7 @@ gulp.task('sass', function () {
             sourceComments: false
         }),
         autoprefixer(),
+        pxtorem(pxtoremOptions, postcssOptions),
         sourcemaps.write('.', {
                 includeContent: false,
                 sourceRoot: config.paths.src + '/assets/css/scss'
